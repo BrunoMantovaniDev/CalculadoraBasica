2a7ab0c3cf7340226492c210ce54fb98bb32f5e6
brunomoraesdmc@gmail.com <>
2022-03-30 11:57:58 -0300

    Adicionando Classe Operações

---
+++ src/main/java/com/mycompany/calculadora/Operacoes.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author bruno
+ */
+public class Operacoes {
+            
+            public static double Soma(double valor1, double valor2){
+                return valor1 + valor2;   
+            };
+            public static double Multiplicacao(double valor1, double valor2){
+                return valor1 + valor2;   
+            };
+            public static double Subtracao(double valor1, double valor2){
+                return valor1 + valor2;   
+            };
+            public static double Divisao(double valor1, double valor2){
+                return valor1 + valor2;   
+            };
+
+}
