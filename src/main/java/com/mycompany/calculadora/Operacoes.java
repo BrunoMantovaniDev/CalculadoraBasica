6cb9e531eaf0c45c2618adde5b8361b75c8660de
bruno <bruno@DESKTOP-4E3HE7F>
2022-05-16 14:19:50 -0300

    Atualizando Switch case

--- src/main/java/com/mycompany/calculadora/Operacoes.java
+++ src/main/java/com/mycompany/calculadora/Operacoes.java
@@ -20,7 +20,15 @@ public class Operacoes {
                 return valor1 + valor2;   
             };
             public static double Divisao(double valor1, double valor2){
-                return valor1 + valor2;   
-            };
+                if(valor2 !=0){
+                        return valor1 / valor2;
+                       
+                    }else{
+                        
+                        throw new IllegalArgumentException("Não é possivel dividir por 0");
+                    }
+            
 
+            }
+            
 }
